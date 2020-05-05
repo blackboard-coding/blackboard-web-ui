40d61be07b1eea654ae7a37a66fadf612e03cf55
Nutthapat Piasangka <n.devs260340@gmail.com>
2020-05-06 00:09:54 +0700

    update

--- src/pages/Bata/index.js
+++ src/pages/Bata/index.js
@@ -2,62 +2,75 @@ import React, { Fragment } from 'react';
 import MobileStoreButton from 'react-mobile-store-button';
 import Icon from './imgs/Logo2_white.png'
 import Background from './imgs/BG_Header.png'
+import { width } from '@material-ui/system';
 function BataPage() {
     const iOSUrl = '';
     const androidUrl = 'https://play.google.com/store/apps/details?id=com.blackboard.th.android';
     return (
-        <div style={{
-            backgroundImage: `url("${Background}")`,
-            height: (window.innerHeight),
-            width: "-webkit-fill-available",
-            backgroundSize: "cover"
-        }}>
+        <Fragment>
             <div style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: '-webkit-fill-available',
+                backgroundImage: `url("${Background}")`,
+                height: (window.innerHeight),
+                width: (window.innerWidth),
+                backgroundSize: "cover"
             }}>
-
                 <div style={{
-                    display: "grid"
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: (window.innerHeight),
                 }}>
-                    <div style={{
-                        margin: "50px"
-                    }}>
-                        <img style={{
-                            paddingLeft: 15,
-                            width: 330
 
-                        }} src={Icon} />
-                    </div>
                     <div style={{
-                        display: 'flex',
-                        justifyContent: 'center',
+                        display: "grid",
+                        width: (window.innerWidth - 10)
                     }}>
-                        <MobileStoreButton
-                            height={75}
-                            width={200}
-                            store="ios"
-                            url={iOSUrl}
-                            linkProps={{ title: 'App Store Button' }}
-                        />
-                    </div>
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                    }}>
-                        <MobileStoreButton
-                            width={230}
-                            height={100}
-                            store="android"
-                            url={androidUrl}
-                            linkProps={{ title: 'Play Store Button' }}
-                        />
+                        <div style={{
+                            margin: "50px 0px"
+                        }}>
+                            <img style={{
+                                paddingLeft: 15,
+                                width: 330
+
+                            }} src={Icon} />
+                        </div>
+                        <div style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                        }}>
+                            <MobileStoreButton
+                                height={75}
+                                width={200}
+                                store="ios"
+                                url={iOSUrl}
+                                linkProps={{ title: 'App Store Button' }}
+                            />
+                        </div>
+                        <div style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                        }}>
+                            <MobileStoreButton
+                                width={230}
+                                height={100}
+                                store="android"
+                                url={androidUrl}
+                                linkProps={{ title: 'Play Store Button' }}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <footer style={{
+                backgroundColor: '#000000',
+                height: '75px',
+
+
+
+            }}>
+
+            </footer>
+        </Fragment >
     )
 }
 
